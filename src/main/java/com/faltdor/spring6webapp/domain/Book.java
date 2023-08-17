a2cca80ddcff85f43cd487cdd68b1c4dff7133b1
wilton <faltdor@gmail.com>
2023-08-16 21:47:59 -0500

    Book store v1

---
+++ src/main/java/com/faltdor/spring6webapp/domain/Book.java
@@ -0,0 +1,48 @@
+package com.faltdor.spring6webapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private String isbn;
+    @ManyToMany(mappedBy = "authors")
+    private Set<Author> authors;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
