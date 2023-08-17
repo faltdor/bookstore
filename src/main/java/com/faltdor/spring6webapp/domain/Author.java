af42335fbec01efac7ba17ef1488e154de22a660
wilton <faltdor@gmail.com>
2023-08-16 21:57:42 -0500

    Add equals and has code to entities

--- src/main/java/com/faltdor/spring6webapp/domain/Author.java
+++ src/main/java/com/faltdor/spring6webapp/domain/Author.java
@@ -2,6 +2,7 @@ package com.faltdor.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -47,5 +48,30 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
